d2017336ab587dcbfc18222a18d88375cb38201d
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2018-10-23 16:42:14 -0500

    Add dummy component

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header-header">
+        <img src={logo} className="Header-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="Header-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Header;
