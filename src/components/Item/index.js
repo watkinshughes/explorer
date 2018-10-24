bb84db34bec33fd009998347906f1c7522671a6f
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2018-10-24 16:17:29 -0500

    Add stopPropagation() to fix toggle

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -12,18 +12,19 @@ class Item extends Component {
     };
   }
   
-  toggleClass() {
+  toggleClass(event) {
       const currentState = this.state.active;
       this.setState({ 
         active: !currentState 
       });
+      event.stopPropagation();
   };
 
   render() {
     return <li className={this.state.active ? 'active': null} 
               onClick={this.toggleClass}>
         <button>
-          { this.props.name } { this.props.type }
+          { this.props.name }
         </button>
         { this.props.children }
     </li>
