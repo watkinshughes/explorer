0588b4f4292e8894d2ac1da05871fb34ca1917ad
Ryan Watkins-Hughes <ryanwh@properinteractive.com>
2018-10-25 18:52:10 -0500

    Add Close button instead of :after on Title for clickability

---
+++ src/components/CloseButton/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './styles.css';
+
+class CloseButton extends Component {
+  render() {
+    return <a className="CloseButton" href="#close"></a>
+  }
+}
+
+export default CloseButton;
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import CloseButton from '../CloseButton';
 import './styles.css';
 
 class Title extends Component {
   render() {
     return <div className="Title">
         { this.props.copy }
+        <CloseButton />
       </div>
   }
 }
